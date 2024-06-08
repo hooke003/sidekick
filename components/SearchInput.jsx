13e1431d345f760c6e9ba2d66391054dfa414d9b
hooke003 <91613022+hooke003@users.noreply.github.com>
2024-06-07 19:29:10 -0500

    resolved empty state error

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -16,7 +16,7 @@ const SearchInput = ({
   return (
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
       <TextInput
-        className="text-base mb-7 text-white flex-1 font-pregular"
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
         value={value}
         placeholder="Search for a video topic"
         placeholderTextColor="#7B7B8B"
