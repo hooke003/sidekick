86b1ed3e5289fda76fff2202f1aec2bb75869698
hooke003 <91613022+hooke003@users.noreply.github.com>
2024-06-07 22:04:55 -0500

    troubles with video card

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,6 +13,7 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import VideoCard from "../../components/VideoCard";
 import { getAllPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 
@@ -33,7 +34,7 @@ const Home = () => {
         data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.title}</Text>
+          <VideoCard video={item}/>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
